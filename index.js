b1d02909cdfb1ce94000f2c0bf07ef7fb8361971
Shynj0 <jpdosanjos0203@gmail.com>
2026-04-10 08:58:35 -0300

    feat: configura pipeline de CI e regras do ESLint

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
-console.log("Aplicativo de Monitoramento de Eventos Climáticos - INPE");
+console.log('Aplicativo de Monitoramento de Eventos Climáticos - INPE');
 //Comentario para ex5 atv3
+const x = 10;
+console.log(x);
